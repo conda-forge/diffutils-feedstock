9028312e760a9b2cbf753db2c12f19db72635084
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-11 15:37:48 -0700

    get debug output

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 ./configure --prefix=${PREFIX} \
     --libdir=${PREFIX}/lib \
     --build=${BUILD} \
-    --host=${HOST}
+    --host=${HOST} || cat config.log
 
 make -j ${CPU_COUNT}
 make install
